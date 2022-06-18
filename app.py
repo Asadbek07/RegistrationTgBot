de17aa22bd13d128090248343d740b59ffa9c83b
Asadbek Sindarov <asindarov07@gmail.com>
2022-06-18 19:21:13 +0500

    solved logic error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from states import Form, AdminForm
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = '5388725901:AAF0dpazNT0hjUoR3rY6V7eyOOEHAqkej5k'
+API_TOKEN = '1710370980:AAGSHs-LlZaTi64cSnsKTE_Pgf-XZWubQtE'
 PHONE_NUM = r'^[\+][0-9]{3}[0-9]{3}[0-9]{6}$'
 CHANNEL_ID = -1001632256119
 
@@ -19,20 +19,43 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+##################
+#  ADMIN HANDLERS
+##################
 
+@dp.message_handler(commands="add_product")
+async def input_new_product(message: types.Message):
+    await AdminForm.product.set()
+    await message.answer(text="Yangi mahsulot qo'shishingiz uchun nomini kiriting!")
+
+@dp.message_handler(state=AdminForm.product)
+async def process_product_name(message: types.Message, state: FSMContext):
+    await state.update_data({
+        "product" : message.text
+    })
+
+    await message.answer("Endi, narxini kiriting!")
+
+    await AdminForm.cost.set()
+
+@dp.message_handler(state=AdminForm.cost)
+async def process_cost(message: types.Message, state: FSMContext):
+    await state.update_data({
+        "cost" : message.text
+    })
+
+    data = await state.get_data()
+    product_name = data.get("product")
+    cost = data.get("cost")
+    added_product = ProductService.add_product(product_name, cost)
+    await message.answer(f"Qo'shgan mahsulotingiz haqida ma'lumot: \n{added_product.name} ({added_product.cost})")
+
+    await state.finish()
+    
 
 ##################
 #  USER HANDLERS
 ##################
-
-@dp.message_handler()
-async def start_handler(message: types.Message):
-    await Form.product.set()
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, selective=True)
-    products = ProductService.get_all_products()
-    markup.add(*(types.KeyboardButton(text=f"{product.name} ({product.cost})") for product in products))
-    await message.answer("Xush kelibsiz! Qaysi mahsulotni tanlashni istaysiz ?", reply_markup=markup)
-
 
 @dp.message_handler(state=Form.product)
 async def process_product_name(message: types.Message, state: FSMContext):
@@ -78,43 +101,22 @@ async def process_phone_number(message : types.Message, state: FSMContext):
     phone_number = data.get("phone_number")
     product = data.get("product")
     await bot.send_message(text=f"{name}| {phone_number}| {product}", chat_id=CHANNEL_ID)
+    
+    await state.finish()
+
     await message.answer("Siz bilan tez orada call center dan bog'lanishadi!")
-    await state.finish()
+
+@dp.message_handler()
+async def start_handler(message: types.Message):
+    await Form.product.set()
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, selective=True)
+    products = ProductService.get_all_products()
+    markup.add(*(types.KeyboardButton(text=f"{product.name} ({product.cost})") for product in products))
+    await message.answer("Xush kelibsiz! Qaysi mahsulotni tanlashni istaysiz ?", reply_markup=markup)
 
 
 
-##################
-#  ADMIN HANDLERS
-##################
 
-@dp.message_handler(commands="add_product")
-async def input_new_product(message: types.Message):
-    await AdminForm.product.set()
-    await message.answer(text="Yangi mahsulot qo'shishingiz uchun nomini kiriting!")
-
-@dp.message_handler(state=AdminForm.product)
-async def process_product_name(message: types.Message, state: FSMContext):
-    await state.update_data({
-        "product" : message.text
-    })
-
-    await message.answer("Endi, narxini kiriting!")
-
-    await AdminForm.cost.set()
-
-@dp.message_handler(state=AdminForm.cost)
-async def process_cost(message: types.Message, state: FSMContext):
-    await state.update_data({
-        "cost" : message.text
-    })
-
-    data = await state.get_data()
-    product_name = data.get("product")
-    cost = data.get("cost")
-    added_product = ProductService.add_product(product_name, cost)
-    await message.answer(f"Qo'shgan mahsulotingiz haqida ma'lumot: \n{added_product.name} ({added_product.cost})")
-
-    await state.finish()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
