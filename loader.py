79faa20bd4f4ead210108bd712c5177bc0790a20
Asadbek Sindarov <asindarov07@gmail.com>
2022-06-18 23:26:14 +0500

    Changed channel ID

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 API_TOKEN = "1710370980:AAGSHs-LlZaTi64cSnsKTE_Pgf-XZWubQtE" # test
 API_TOKEN = "5354713153:AAF0THDIksTs6IUl7D7ctpL2YUFKRbuEQKE" # production
 PHONE_NUM = r'^[\+][0-9]{3}[0-9]{3}[0-9]{6}$'
-CHANNEL_ID = -1001632256119
+CHANNEL_ID = -1001576660374
 
 bot = Bot(token=API_TOKEN)
 
