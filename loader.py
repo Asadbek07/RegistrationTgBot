9d3f3fa60844acd5d74115bcc4f941a948d9f725
Asadbek Sindarov <asindarov07@gmail.com>
2022-06-20 11:24:08 +0500

    Code rub

--- loader.py
+++ loader.py
@@ -1,10 +1,9 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher
 
-# API_TOKEN = "1710370980:AAGSHs-LlZaTi64cSnsKTE_Pgf-XZWubQtE" # test
-API_TOKEN = "5354713153:AAF0THDIksTs6IUl7D7ctpL2YUFKRbuEQKE" # production
+API_TOKEN = "API TOKEN" # production
 PHONE_NUM = r'^[\+][0-9]{3}[0-9]{3}[0-9]{6}$'
-CHANNEL_ID = -1001576660374
+CHANNEL_ID = 0 # channel id
 
 bot = Bot(token=API_TOKEN)
 
